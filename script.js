01c1441d0834b09bc27930327b625579b84b0ed2
acolote1998 <manuelacevedo.ar@gmail.com>
2026-01-06 20:19:04 +0100

    adding dynamic ingredient loading

--- script.js
+++ script.js
@@ -1,87 +1,68 @@
-function calculateNutrients(nutrientP100g,gramsnutrientP){
-return gramsnutrientP * nutrientP100g / 100;
+function calculateNutrients(nutrientP100g, grams) {
+  return grams * nutrientP100g / 100;
 }
 
-function calculateCalories(){
+// Agregar nuevo ingrediente
+function addIngredient() {
+  const container = document.getElementById('ingredients');
+  const count = container.children.length + 1;
 
-	return   calculateNutrients(parseFloat(document.getElementById('caloriesi1').value),parseFloat(document.getElementById('gramsi1').value))
-            +calculateNutrients(parseFloat(document.getElementById('caloriesi2').value),parseFloat(document.getElementById('gramsi2').value))
-            +calculateNutrients(parseFloat(document.getElementById('caloriesi3').value),parseFloat(document.getElementById('gramsi3').value))
-            +calculateNutrients(parseFloat(document.getElementById('caloriesi4').value),parseFloat(document.getElementById('gramsi4').value));
+  const div = document.createElement('div');
+  div.className = 'ingredient';
+  div.innerHTML = `
+    <h3>Ingredient ${count}<br><input type="text" class="iname"></h3>
+
+    <label>Grams:</label><br>
+    <input type="number" class="grams" value="0"><br><br>
+
+    <label>Calories per 100g:</label><br>
+    <input type="number" class="calories" value="0"><br><br>
+
+    <label>Carbs per 100g:</label><br>
+    <input type="number" class="carbs" value="0"><br><br>
+
+    <label>Protein per 100g:</label><br>
+    <input type="number" class="protein" value="0"><br><br>
+
+    <label>Fat per 100g:</label><br>
+    <input type="number" class="fat" value="0"><br><br>
+    <hr>
+  `;
+
+  container.appendChild(div);
 }
 
-function calculateCarbs(){
- 
-    return  calculateNutrients(parseFloat(document.getElementById('carbsi1').value),parseFloat(document.getElementById('gramsi1').value))
-        +   calculateNutrients(parseFloat(document.getElementById('carbsi2').value),parseFloat(document.getElementById('gramsi2').value))
-        +   calculateNutrients(parseFloat(document.getElementById('carbsi3').value),parseFloat(document.getElementById('gramsi3').value))
-        +   calculateNutrients(parseFloat(document.getElementById('carbsi4').value),parseFloat(document.getElementById('gramsi4').value));
-}
+// Cálculo general (recorre todos los ingredientes)
+function calculate() {
+  const ingredients = document.querySelectorAll('.ingredient');
 
-function calculateProteins(){
- 
-    return  calculateNutrients(parseFloat(document.getElementById('proteini1').value),parseFloat(document.getElementById('gramsi1').value))
-        +   calculateNutrients(parseFloat(document.getElementById('proteini2').value),parseFloat(document.getElementById('gramsi2').value))
-        +   calculateNutrients(parseFloat(document.getElementById('proteini3').value),parseFloat(document.getElementById('gramsi3').value))
-        +   calculateNutrients(parseFloat(document.getElementById('proteini4').value),parseFloat(document.getElementById('gramsi4').value));
-}
+  let totalGrams = 0;
+  let totalCalories = 0;
+  let totalCarbs = 0;
+  let totalProtein = 0;
+  let totalFat = 0;
 
-function calculateFats(){
+  ingredients.forEach(ing => {
+    const grams = parseFloat(ing.querySelector('.grams').value) || 0;
+    const calories = parseFloat(ing.querySelector('.calories').value) || 0;
+    const carbs = parseFloat(ing.querySelector('.carbs').value) || 0;
+    const protein = parseFloat(ing.querySelector('.protein').value) || 0;
+    const fat = parseFloat(ing.querySelector('.fat').value) || 0;
 
-    return  calculateNutrients(parseFloat(document.getElementById('fati1').value),parseFloat(document.getElementById('gramsi1').value))
-        +   calculateNutrients(parseFloat(document.getElementById('fati2').value),parseFloat(document.getElementById('gramsi2').value))
-        +   calculateNutrients(parseFloat(document.getElementById('fati3').value),parseFloat(document.getElementById('gramsi3').value))
-        +   calculateNutrients(parseFloat(document.getElementById('fati4').value),parseFloat(document.getElementById('gramsi4').value));
-}
+    totalGrams += grams;
+    totalCalories += calculateNutrients(calories, grams);
+    totalCarbs += calculateNutrients(carbs, grams);
+    totalProtein += calculateNutrients(protein, grams);
+    totalFat += calculateNutrients(fat, grams);
+  });
 
-function calculatePortionGrams(){
-
-    const gramsportion1 = parseFloat(document.getElementById('gramsi1').value);
-    const gramsportion2 = parseFloat(document.getElementById('gramsi2').value);
-    const gramsportion3 = parseFloat(document.getElementById('gramsi3').value);
-    const gramsportion4 = parseFloat(document.getElementById('gramsi4').value);
-	const resultgramsportiontotal = gramsportion1+gramsportion2+gramsportion3+gramsportion4;
-
-	return resultgramsportiontotal;
-}
-
-function calculate(){
-
-
-    // Display the result in the result textbox
-    console.log("Ingredient 1:")
-    console.log("Calories:"+calculateNutrients(parseFloat(document.getElementById('caloriesi1').value),parseFloat(document.getElementById('gramsi1').value)));
-    console.log("Carbs:"+calculateNutrients(parseFloat(document.getElementById('carbsi1').value),parseFloat(document.getElementById('gramsi1').value)));
-    console.log("Fat:"+calculateNutrients(parseFloat(document.getElementById('fati1').value),parseFloat(document.getElementById('gramsi1').value)));
-    console.log("Protein:"+calculateNutrients(parseFloat(document.getElementById('proteini1').value),parseFloat(document.getElementById('gramsi1').value)));
-
-    console.log("Ingredient 2:")
-    console.log("Calories:"+calculateNutrients(parseFloat(document.getElementById('caloriesi2').value),parseFloat(document.getElementById('gramsi2').value)));
-    console.log("Carbs:"+calculateNutrients(parseFloat(document.getElementById('carbsi2').value),parseFloat(document.getElementById('gramsi2').value)));
-    console.log("Fat:"+calculateNutrients(parseFloat(document.getElementById('fati2').value),parseFloat(document.getElementById('gramsi2').value)));
-    console.log("Protein:"+calculateNutrients(parseFloat(document.getElementById('proteini2').value),parseFloat(document.getElementById('gramsi2').value)));
-
-    console.log("Ingredient 3:")
-    console.log("Calories:"+calculateNutrients(parseFloat(document.getElementById('caloriesi3').value),parseFloat(document.getElementById('gramsi3').value)));
-    console.log("Carbs:"+calculateNutrients(parseFloat(document.getElementById('carbsi3').value),parseFloat(document.getElementById('gramsi3').value)));
-    console.log("Fat:"+calculateNutrients(parseFloat(document.getElementById('fati3').value),parseFloat(document.getElementById('gramsi3').value)));
-    console.log("Protein:"+calculateNutrients(parseFloat(document.getElementById('proteini3').value),parseFloat(document.getElementById('gramsi3').value)));
-
-    console.log("Ingredient 4:")
-    console.log("Calories:"+calculateNutrients(parseFloat(document.getElementById('caloriesi4').value),parseFloat(document.getElementById('gramsi4').value)));
-    console.log("Carbs:"+calculateNutrients(parseFloat(document.getElementById('carbsi4').value),parseFloat(document.getElementById('gramsi4').value)));
-    console.log("Fat:"+calculateNutrients(parseFloat(document.getElementById('fati4').value),parseFloat(document.getElementById('gramsi4').value)));
-    console.log("Protein:"+calculateNutrients(parseFloat(document.getElementById('proteini4').value),parseFloat(document.getElementById('gramsi4').value)));
-
-
-
-    document.getElementById('resultname').value = document.getElementById('foodname').value;
-    document.getElementById('resultbarcodename').value = document.getElementById('barcode').value;
-    document.getElementById('resultbarcode').value = "*"+document.getElementById('barcode').value;+"*";
-    document.getElementById('portiongrams').value = calculatePortionGrams() +" grams";
-    document.getElementById('resultcaloriesportion').value = calculateCalories() +" calories";
-    document.getElementById('resultcalories').value = 100 * calculateCalories() / calculatePortionGrams() +" calories";
-    document.getElementById('resultcarbs').value = 100 * calculateCarbs() / calculatePortionGrams() +" g";
-    document.getElementById('resultprotein').value = 100 * calculateProteins() /calculatePortionGrams() +" g";
-    document.getElementById('resultfat').value = 100 * calculateFats() /calculatePortionGrams() +" g";
+  document.getElementById('resultname').value = document.getElementById('foodname').value;
+  document.getElementById('resultbarcodename').value = document.getElementById('barcode').value;
+  document.getElementById('resultbarcode').value = "*" + document.getElementById('barcode').value + "*";
+  document.getElementById('portiongrams').value = totalGrams + " grams";
+  document.getElementById('resultcaloriesportion').value = totalCalories + " calories";
+  document.getElementById('resultcalories').value = (100 * totalCalories / totalGrams).toFixed(2);
+  document.getElementById('resultcarbs').value = (100 * totalCarbs / totalGrams).toFixed(2);
+  document.getElementById('resultprotein').value = (100 * totalProtein / totalGrams).toFixed(2);
+  document.getElementById('resultfat').value = (100 * totalFat / totalGrams).toFixed(2);
 }
